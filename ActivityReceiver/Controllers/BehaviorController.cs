415158867fcdb814214763aa6f65b2250976c5e6
Xueliang Chen <Neko3000@live.com>
2018-04-09 10:58:33 +0900

    feat(BehaviorController): add BehaviorController with its dto, build up api function basically

---
+++ ActivityReceiver/Controllers/BehaviorController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ActivityReceiver.Data;
+using ActivityReceiver.Models;
+
+namespace ActivityReceiver.Controllers
+{
+    [Produces("application/json")]
+    [Route("behavior")]
+    public class BehaviorController : Controller
+    {
+        private ActivityReceiverDbContext _arDbContext;
+
+        public BehaviorController(ActivityReceiverDbContext arDbContext)
+        {
+            _arDbContext = arDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var movements = _arDbContext.Movements.ToList();
+
+            return Ok(movements);
+        }
+    }
+}
---
+++ ActivityReceiver/Models/DataTransferObject/MovementDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActivityReceiver.Models.DataTransferObject
+{
+    public class MovementDto
+    {
+        public int ID { get; set; }
+        public int QID { get; set; }
+        public int UID { get; set; }
+        public int Index { get; set; }
+        public int State { get; set; }
+        public int Time { get; set; }
+        public int XPosition { get; set; }
+        public int YPostion { get; set; }
+        public bool IsFinished { get; set; }
+    }
+}
