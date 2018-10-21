ad3ff6f4f378e2b3a0635d9e676a63f69503edc5
Neko3000 <Neko3000@live.com>
2018-10-21 11:43:05 +0900

    feat(QuestionController): return the list of questions for mobile app

---
+++ ActivityReceiver/Controllers/QuestionController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ActivityReceiver.Data;
+using ActivityReceiver.Models;
+
+namespace ActivityReceiver.Controllers
+{
+    [Produces("application/json")]
+    [Route("question")]
+    public class QuestionController : Controller
+    {
+        private ActivityReceiverDbContext _arDbContext;
+
+        public QuestionController(ActivityReceiverDbContext arDbContext)
+        {
+            _arDbContext = arDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var questions = _arDbContext.Questions.ToList();
+
+            return Ok(questions);
+        }
+    }
+}
